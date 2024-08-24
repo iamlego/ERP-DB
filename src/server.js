a638a46bf774cd38cec2e883099fb73026eedcc9
saairajj <saairajj812@gmail.com>
2024-08-24 12:41:26 -0700

    Revert "server.js updated to test on new feature branch"
    This reverts commit 4b0f352c696fcfe15210f50defa16778165e5343.

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const PORT = db.port || 3000;
 // const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT} and features are from a new Branch`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 
